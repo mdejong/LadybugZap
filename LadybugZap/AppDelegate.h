cd3e63d1dd2c81fc153bb76a98027763d7497939
Moses DeJong <mo@moses-dejongs-macbook-2.local>
2013-06-17 13:17:08 -0700

    Initial Commit

---
+++ LadybugZap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LadybugZap
+//
+//  Created by Moses DeJong on 6/17/13.
+//  Copyright (c) 2013 HelpURock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LadybugZap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LadybugZap
+//
+//  Created by Moses DeJong on 6/17/13.
+//  Copyright (c) 2013 HelpURock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
